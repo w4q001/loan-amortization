157879938f0efb187429e1639532dccef4d126a9
w4q001 <wkwk09207@gmail.com>
2025-08-21 16:36:18 +0100

    loan amortization.py

--- loan amortization.py
+++ loan amortization.py
@@ -1,8 +1,19 @@
-principal = int(input('enter the sum of money to be returned: £'))
-interest_rate = int(input('enter the interest rate on this loan: %'))
+
+principal = float(input('enter the sum of money to be returned: £'))
+interest_rate = float(input('enter the interest rate on this loan: %'))
 interest_rate = interest_rate/100
-n = int(input('enter how long you have to pay this loan in months: '))
+n = float(input('enter how long you have to pay this loan in months: '))
 r = interest_rate/12
 
-A = principal * (r*(1+r)**n)/(-1 + (1+r)**n)
-print(f'your monthly payment is £{A:.2f}')
+if interest_rate == 0:
+    A = principal/n
+else:
+    A = principal * (r * (1 + r) ** n) / (-1 + (1 + r) ** n)
+
+
+total_payment = A * n
+total_interest = total_payment - principal
+
+print(f'\nMonthly payment: £{A:.2f}')
+print(f'Total repayment: £{total_payment:.2f}')
+print(f'Total interest paid: £{total_interest:.2f}')
